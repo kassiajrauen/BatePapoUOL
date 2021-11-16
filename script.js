64fe2d2f8ab38ec9c5019197b6945abeb1f1957d
kassiajrauen <kassiajrauen@gmail.com>
2021-11-15 23:59:12 -0300

    implementação funcionalidade enviar mensagem

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
 let messages = []
 
-// // const nickname = prompt("Digite seu nickname: ")
-// const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", { name: nickname })
-//     .then()
-//     .catch(
-//         function () {
-//             alert('Esse nome já existe, tente outro :)');
-//             document.location.reload(true);
-//         }
-//     );
-
-// promiseStatus(true);
-
-// function promiseStatus(initiate) {
-//     if (initiate !== true) {
-//         axios.post("https://mock-api.driven.com.br/api/v4/uol/status", { name: nickname })
-//             .then()
-//             .catch(
-//                 function () {
-//                     alert('Sua sessão expirou! Entre novamente ;)');
-//                     document.location.reload(true);
-//                 }
-//             );
-//     };
-//     setTimeout(promiseStatus, 5000);
-// };
-
-const loadMessage = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+const nickname = prompt("Digite seu nickname: ")
+const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", { name: nickname })
     .then(
-        response => {
-            messages = [...messages, ...response.data]
-            showMessages()
-        }
+        (response) => response.status_code
     )
+    .catch(
+        () => {
+            alert('Esse nome já existe, tente outro :)');
+            document.location.reload(true);
+        }
+    );
+
+// TODO: VERIFICAR A NECESSIDADE DESSA FUNÇÃO
+if (promise === 200) {
+    const payload = {
+        from: nickname,
+        to: "Todos",
+        text: "entrou na sala...",
+        type: "status"
+    }
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", payload)
+}
+
+promiseStatus(true);
+
+function promiseStatus(initiate) {
+    if (initiate !== true) {
+        axios.post("https://mock-api.driven.com.br/api/v4/uol/status", { name: nickname })
+            .then()
+            .catch(
+                () => {
+                    alert('Sua sessão expirou! Entre novamente ;)');
+                    document.location.reload(true);
+                }
+            );
+    };
+    setTimeout(promiseStatus, 5000);
+};
+
+loadMessage(true);
+
+function loadMessage(initiate) {
+    if (initiate !== true) {
+        axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+            .then(
+                response => {
+                    messages = [...messages, ...response.data]
+                    showMessages()
+                }
+            )
+    };
+    setTimeout(loadMessage, 3000)
+};
 
 function showMessages() {
     let container = document.querySelector(".container")
@@ -51,10 +71,17 @@ function showMessages() {
 
 function sendMsg() {
     const message = document.querySelector(".text-area").value
-
-    console.log(message)
-
-    //    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", message)
+    const payload = {
+        from: nickname,
+        to: "Todos",
+        text: message,
+        type: "message"
+    }
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", payload)
+        .then(
+            (r) => console.log(r.data)
+        )
+    // TODO: fazer limpar a barra de escrever
 }
 
 
